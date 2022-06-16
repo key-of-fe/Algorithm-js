2ceed0d20ebbb124a76e75073aad5140d6714003
한지희 <57996351+g2hhh2ee@users.noreply.github.com>
2022-06-16 11:52:10 +0900

    6월 2주차

--- programmers/jihee/행렬 테두리 회전하기/P77485_행렬테두리회전하기.js
+++ programmers/jihee/행렬 테두리 회전하기/P77485_행렬테두리회전하기.js
@@ -1,4 +1,32 @@
+// 주비님 코드 참고
 function solution(rows, columns, queries) {
-    let answer = [];
-    return answer;
+    const board = Array(rows).fill().map((row, i) => Array(columns).fill().map((column, j) => i*columns + j + 1));
+ 
+    const rotate = function(query){
+        let [x1, y1, x2, y2] = query;
+        x1--, y1--, x2--, y2--;
+ 
+        const ft = board[x1][y1];
+        const arr = [ft];
+        for(let i = x1; i < x2; i++){
+            arr.push(board[i][y1] = board[i+1][y1]);
+        }
+        for(let j = y1; j < y2; j++){
+            arr.push(board[x2][j] = board[x2][j+1]);
+        }
+        for(let i = x2; i > x1; i--){
+            arr.push(board[i][y2] = board[i-1][y2]);
+        }
+        for(let j = y2; j > y1; j--){
+            arr.push(board[x1][j] = board[x1][j-1]);
+        }
+        board[x1][y1+1] = ft;
+ 
+        return Math.min(...arr);
+    }
+ 
+    return queries.reduce((ans, query) => {
+        ans.push(rotate(query));
+        return ans;
+    }, []);
 }
